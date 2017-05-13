e839d929b0beafde7586ccbafced97a17505a181
Dinesh Bagga <dinesh31191@gmail.com>
2017-05-14 01:33:04 +0530

    Code refactored and sometimes event.key is undefined, therefore using event.keycode for the same.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener('keyup', function (event) {
     var key = event.key,
         scrollPos = window.innerHeight;
-    if (key === "ArrowDown") {
+    if (key === "ArrowDown" || event.keyCode === 40) {
         event.preventDefault();
         window.scrollBy(0, scrollPos);
     }
-    if (key === "ArrowUp") {
+    if (key === "ArrowUp" || event.keyCode === 38) {
         event.preventDefault();
         window.scrollBy(0, -scrollPos);
     }
